32b90cab200fa89042375666d82d6a800956be40
acalim98 <ncalim@sfo.yearup.org>
2023-01-19 09:56:50 -0800

    Angelo;s commit

---
+++ registration.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('registertrationForm')
+
+function registerUser(event) {
+
+
+
+ //commentupdate   event.preventDefault(); 
+
+    console.log(document.getElementById("username").value)
+    console.log(document.getElementById("password").value)
+    console.log(document.getElementById("fullName").value)
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "username": document.getElementById("username").value,
+        "password": document.getElementById("password").value,
+        "fullName": document.getElementById("fullName").value,
+        
+    });
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    fetch("https://microbloglite.herokuapp.com/api/users", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
+form.addEventListener('submit', function () {
+    console.log("formsubmit")
+})
+
+let signup = document.getElementById("signup")
+
+
+signup.addEventListener("click", registerUser)
